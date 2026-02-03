7252ce8e8cf404e8e3d6a64c9a01b357ada9aac2
Vishisht_Dwivedi <170785484+Vishisht-Dwivedi@users.noreply.github.com>
2026-02-03 11:48:50 +0530

    length > 0

---
+++ Lab-1/StringLengthGreaterThan0.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    char s[256];
+    scanf("%255s", s);
+
+    int q0 = 1, q1 = 0;
+
+    for(int i = 0; s[i] != '\0'; i++) {
+        int new_q0 = 0, new_q1 = 0;
+
+        if(q0) {
+            if(s[i] == '0' || s[i] == '1') {
+                new_q0 = 1;
+                new_q1 = 1;
+            }
+        }
+
+        if(q1) {
+            if(s[i] == '0' || s[i] == '1')
+                new_q1 = 1;
+        }
+
+        q0 = new_q0;
+        q1 = new_q1;
+    }
+
+    if(q1)
+        printf("Accepted\n");
+    else
+        printf("Rejected\n");
+
+    return 0;
+}
